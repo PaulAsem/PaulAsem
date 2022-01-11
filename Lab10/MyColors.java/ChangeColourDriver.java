65acfd44e638074c0678c651b2de1a0c7935ec94
PaulAsem <91595575+PaulAsem@users.noreply.github.com>
2022-01-11 09:46:39 +0000

    Add files via upload

---
+++ Lab10/MyColors.java/ChangeColourDriver.java
@@ -0,0 +1,16 @@
+package MyColors.java;
+
+import javax.swing.JFrame;
+
+public class ChangeColourDriver {
+
+	public static void main(String[] args) {
+	
+		ChangeColour changeColour = new ChangeColour("Button");
+		changeColour.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		changeColour.setSize(200, 200);
+		changeColour.setVisible(true);
+
+	}//end main
+
+}//end class
